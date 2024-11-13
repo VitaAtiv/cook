9e7326f5179264c7d4b47b0df7ad41c6dfdee9ab
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-11-13 18:31:23 +0200

    add pr

--- modal.js
+++ modal.js
@@ -15,7 +15,7 @@
 //     }
 //   })();
 
-  const modal = document.querySelector(".modal");
+  const modal = document.querySelector(".backdrop");
   const modalBtnOpen = document.querySelector(".modal-btn-open");
   const modalBtnClose = document.querySelector(".modal-btn-close");
 
